2205a47a687cfef66cbea1d378f3c822886b3946
Kole Reiser <k.reiser@setonhill.edu>
2023-04-26 14:37:01 -0400

    done

--- 7/ViewController.swift
+++ 7/ViewController.swift
@@ -12,8 +12,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        labelPens.isUserInteractionEnabled = true
+        pens.isUserInteractionEnabled = true
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.tap(recognizer:)))
+        tapGesture.numberOfTouchesRequired = 1
+        tapGesture.numberOfTapsRequired = 5
+        labelPens.addGestureRecognizer(tapGesture)
+        
+        let swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(self.swipe(recognizer:)))
+        swipeGesture.direction = .up
+        pens.addGestureRecognizer(swipeGesture)
     }
 
-
+    @IBOutlet weak var pens: UIImageView!
+    
+    @IBOutlet weak var labelPens: UILabel!
+    
+    
+    @objc func tap(recognizer:UITapGestureRecognizer) {
+        labelPens.backgroundColor = .orange
+    }
+    @objc func swipe(recognizer:UISwipeGestureRecognizer) {
+        pens.image = UIImage(named: "steelers")
+    }
+    
 }
-
